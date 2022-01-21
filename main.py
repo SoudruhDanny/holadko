9f056777c4b3753347b5cae44da98ffe021b29e5
Jimysuck <schrotter.tom@gmail.com>
2022-01-21 23:17:21 +0100

    fixed sell

--- main.py
+++ main.py
@@ -3,64 +3,21 @@ import oauthy
 import logging
 from time import sleep
 import pyautogui
-#import keyboard
+# import keyboard
 import re
-#from operator import methodcaller
+# from operator import methodcaller
 from pynput.keyboard import Controller
 
 KEYBOARD = Controller()
 
-
 #
-#Getting your credentials
+# Getting your credentials
 server = 'irc.chat.twitch.tv'
 port = 6667
 nickname = 'jimyslave'
-token = oauthy.oauth  #test oauth
+token = oauthy.oauth  # test oauth
 channel = '#jimysak'
 
-board_map = {
-    1: (549, 446),
-    2: (681, 446),
-    3: (792, 446),
-    4: (908, 446),
-    5: (1020, 446),
-    6: (1142, 446),
-    7: (1255, 446),
-    8: (598, 520),
-    9: (718, 520),
-    10: (838, 520),
-    11: (972, 520),
-    12: (1074, 520),
-    13: (1214, 520),
-    14: (1324, 520),
-    15: (547, 593),
-    16: (662, 593),
-    17: (782, 593),
-    18: (897, 593),
-    19: (1026, 593),
-    20: (1153, 593),
-    21: (1273, 593),
-    22: (587, 676),
-    23: (683, 676),
-    24: (823, 676),
-    25: (961, 676),
-    26: (1084, 676),
-    27: (1222, 676),
-    28: (1337, 676)
-}
-
-deck_map = {
-    1: (433, 787),
-    2: (546, 787),
-    3: (655, 787),
-    4: (771, 787),
-    5: (889, 787),
-    6: (1012, 787),
-    7: (1129, 787),
-    8: (1254, 787),
-    9: (1356, 787)
-}
 
 item_map = {
     1: (335, 597),
@@ -75,8 +32,8 @@ item_map = {
     10: (290, 757)
 }
 
-item_move = {
-    1: (549, 446),  #Board
+map_all = {
+    1: (549, 446),  # Board
     2: (681, 446),
     3: (792, 446),
     4: (908, 446),
@@ -103,8 +60,8 @@ item_move = {
     25: (961, 676),
     26: (1084, 676),
     27: (1222, 676),
-    28: (1337, 676),  #End of board
-    29: (433, 787),   #Deck
+    28: (1337, 676),  # End of board
+    29: (433, 787),  # Deck
     30: (546, 787),
     31: (655, 787),
     32: (771, 787),
@@ -112,7 +69,7 @@ item_move = {
     34: (1012, 787),
     35: (1129, 787),
     36: (1254, 787),
-    37: (1356, 787)  #End of deck
+    37: (1356, 787)  # End of deck
 }
 
 
@@ -142,32 +99,10 @@ def augment(value):
         sleep(0.06)
 
 
-def move_champion_to(deck, board):
-    pyautogui.moveTo(deck_map[int(deck)])
-    sleep(.050)
-    pyautogui.mouseDown(button='left')
-    sleep(.050)
-    pyautogui.moveTo(board_map[int(board)])
-    sleep(.050)
-    pyautogui.mouseUp(button='left')
-    print('Done')
-
-
 def lock_shop():
     pyautogui.moveTo(1450, 904)
     sleep(0.05)
     pyautogui.mouseDown(button='left')
-    sleep(.050)
-    pyautogui.mouseUp(button='left')
-    print('Done')
-
-
-def move_champion_from(deck, board):
-    pyautogui.moveTo(board_map[int(board)])
-    sleep(.050)
-    pyautogui.mouseDown(button='left')
-    sleep(.050)
-    pyautogui.moveTo(deck_map[int(deck)])
     sleep(.050)
     pyautogui.mouseUp(button='left')
     print('Done')
@@ -234,7 +169,7 @@ def shop(value: int):
 
 
 def sell(deck):
-    pyautogui.moveTo(deck_map[int(deck)])
+    pyautogui.moveTo(map_all[int(deck)])
     sleep(.050)
     pyautogui.mouseDown(button='left')
     sleep(.050)
@@ -244,12 +179,12 @@ def sell(deck):
     print('Done')
 
 
-def move(board1, board2):
-    pyautogui.moveTo(board_map[int(board1)])
+def move(from_value, to_value):
+    pyautogui.moveTo(map_all[int(from_value)])
     sleep(.050)
     pyautogui.mouseDown(button='left')
     sleep(.050)
-    pyautogui.moveTo(board_map[int(board2)])
+    pyautogui.moveTo(map_all[int(to_value)])
     sleep(.050)
     pyautogui.mouseUp(button='left')
     print('Done')
@@ -260,7 +195,7 @@ def move_item(item, number):
     sleep(.050)
     pyautogui.mouseDown(button='left')
     sleep(.050)
-    pyautogui.moveTo(item_move[int(number)])
+    pyautogui.moveTo(map_all[int(number)])
     sleep(.050)
     pyautogui.mouseUp(button='left')
     print('Done')
@@ -312,18 +247,6 @@ def main():
                     if int(value) in range(1, 6):
                         shop(int(value))
 
-                if "!toBoard" in resp:
-                    raw_value = re.findall(r'\s\d{1,2}', resp)
-                    deck = raw_value[0]
-                    board = raw_value[1]
-                    move_champion_to(int(deck), int(board))
-
-                if '!toDeck' in resp:
-                    raw_value = re.findall(r'\s\d{1,2}', resp)
-                    deck = raw_value[1]
-                    board = raw_value[0]
-                    move_champion_from(int(deck), int(board))
-
                 if '!sell' in resp:
                     raw_value = re.search(r'\s\d', resp)
                     value = raw_value.group()
@@ -332,9 +255,9 @@ def main():
 
                 if '!move' in resp:
                     raw_value = re.findall(r'\s\d{1,2}', resp)
-                    board1 = raw_value[0]
-                    board2 = raw_value[1]
-                    move(board1, board2)
+                    from_value = raw_value[0]
+                    to_value = raw_value[1]
+                    move(from_value, to_value)
 
                 if '!item' in resp:
                     raw_value = re.findall(r'\s\d{1,2}', resp)
